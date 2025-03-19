fcd780d917403e985364503ed593b342710f5a78
Amirhosein Ghaffarian <amirhoseinghf82@gmail.com>
2025-03-19 13:28:39 +0330

    Added flags and logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"log"
+	"flag"
 	"net/http"
+
+	"github.com/charmbracelet/log"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "Http network address")
+	flag.Parse()
+
+	// infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,7 +24,12 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting Server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	srv := &http.Server{
+		Addr:    *addr,
+		Handler: mux,
+	}
+
+	log.Infof("Starting Server on %s", *addr)
+	err := srv.ListenAndServe()
 	log.Fatal(err)
 }
